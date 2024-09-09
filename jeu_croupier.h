021e6679612f9af419910e2c36ff80ed99a0d960
J.Chris <amevorjeanchristophe@gmail.com>
2024-09-09 02:03:01 +0000

    premiere partie opérationnelle du jeu blackkack et qui gère grace aux liste chainées les cartes. Ce commit renferme la partie du Jeu sensé gérer le comportement du croupier et la repartition initiale des cartes

--- jeu_croupier.h
+++ jeu_croupier.h
@@ -1,6 +1,8 @@
 #ifndef __JEU_CROUPIER__
 #define __JEU_CROUPIER__
 
+
+
 typedef struct Card {
     int number;  // De 1 à 13 pour les valeurs (As, 2, 3, ..., Roi)
     int color;  // De 0 à 3 pour les couleurs (Cœur, Carreau, Trèfle, Pique)
@@ -13,17 +15,43 @@ typedef struct {
 typedef struct Liste_carte
 {
     card carte ;
-    liste_carte* suivant = NULL ;
+    struct Liste_carte *suivant  ;
 }liste_carte;
 
 typedef struct{
-    liste_carte * tete = NULL;
+    liste_carte * tete ;
 } tete ;
 
-tete pre_elmt ;
+typedef struct Joueur{
+    card carte1 ;
+    card carte2 ;
+} joueur ;
+
+typedef struct Croupier{
+    card carte1 ;
+    card carte2 ;
+    struct Carte_suppl * carte_suivante ;   // permet de stocker les autres cartes au cas où le croupier aura a en tirer
+    int val ;
+}croupier ;   
+
+typedef struct Carte_suppl{
+    card carte ;
+    struct Carte_suppl *next ;
+} carte_suppl ;
+
 
 int valeur_card(int indice_card) ;
 
-card distribution_carte(liste_carte liste) ;
+tete creation_liste_carte(void) ;
+
+void libération_memoire(tete pre_elmt);
+
+card distribution_une_seule_carte(tete tete_list ) ;
+
+void comportement_croupier(croupier *crou_pier);
+
+void affichage_des_cartes(card carte) ;
+
+int taille_liste(tete liste) ;
 
 #endif
